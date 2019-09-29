4d2e9097e807010a8f90c5ca6a6ac8fdb3fbe7bd
Wayne Sun <wtse.sun@gmail.com>
2019-09-28 19:03:04 -0700

    deleted counter.js counterHook.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React, { useState } from 'react';
-import Counter from './Counter'
-import CounterHook from './CounterHook';
-
-export const ThemeContext = React.createContext(); // provider & consumer
 
 function App() {
-  const [theme, setTheme] = useState('green');
-  return (
-    <ThemeContext.Provider value={{ backgroundColor: theme}}>
-      {console.log('App is rendered')}
-      <Counter initialCount={0}/>
-      <CounterHook initialCount={0} />
-      <button onClick={() => setTheme(prevTheme => 
-        prevTheme === 'red' ? 'blue' : 'red'
-        )}>Toggle Theme</button>
-    </ThemeContext.Provider>
-  )
+  return null;
 }
 
 export default App;
